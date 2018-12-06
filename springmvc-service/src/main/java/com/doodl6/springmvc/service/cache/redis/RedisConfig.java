34dea89a81e76b266291e516466040991d75e145
daixiaoming <daixiaoming@raycloud.com>
2018-12-06 17:52:34 +0800

    添加redis文件配置

--- springmvc-service/src/main/java/com/doodl6/springmvc/service/cache/redis/RedisConfig.java
+++ springmvc-service/src/main/java/com/doodl6/springmvc/service/cache/redis/RedisConfig.java
@@ -5,8 +5,10 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -20,19 +22,38 @@ import java.time.Duration;
  * Created by daixiaoming on 2018-12-05.
  */
 @Configuration
+@PropertySource("classpath:redis.properties")
 public class RedisConfig {
+
+    @Value("${hostName}")
+    private String hostName;
+
+    @Value("${port}")
+    private int port;
+
+    @Value("${pool.maxIdle}")
+    private int maxIdle;
+
+    @Value("${pool.minIdle}")
+    private int minIdle;
+
+    @Value("${pool.maxWaitMillis}")
+    private int maxWaitMillis;
+
+    @Value("${pool.maxTotal}")
+    private int maxTotal;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName("127.0.0.1");
-        redisStandaloneConfiguration.setPort(6379);
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
 
         GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
-        poolConfig.setMaxIdle(10000);
-        poolConfig.setMinIdle(10000);
-        poolConfig.setMaxWaitMillis(5000);
-        poolConfig.setMaxTotal(2);
+        poolConfig.setMaxIdle(maxIdle);
+        poolConfig.setMinIdle(minIdle);
+        poolConfig.setMaxWaitMillis(maxWaitMillis);
+        poolConfig.setMaxTotal(maxTotal);
 
         LettucePoolingClientConfiguration lettucePoolingClientConfiguration = LettucePoolingClientConfiguration.builder()
                 .commandTimeout(Duration.ofSeconds(10))
