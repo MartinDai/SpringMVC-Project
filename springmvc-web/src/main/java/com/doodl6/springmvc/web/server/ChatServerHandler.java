cfcf185071a466ed123de65afd3f75c841a7594c
MartinDai <xiaomingys@gmail.com>
2019-03-02 21:19:52 +0800

    netty聊天服务调整

--- springmvc-web/src/main/java/com/doodl6/springmvc/web/server/ChatServerHandler.java
+++ springmvc-web/src/main/java/com/doodl6/springmvc/web/server/ChatServerHandler.java
@@ -3,7 +3,6 @@ package com.doodl6.springmvc.web.server;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -14,48 +13,45 @@ import java.util.Map;
 @Sharable
 public class ChatServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
-    private static Map<Channel, String> userChannelMap = Maps.newHashMap();
+    private static Map<ChannelHandlerContext, String> userChannelMap = Maps.newHashMap();
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
-        Channel currentChannel = ctx.channel();
         JSONObject messageJSON = JSON.parseObject(msg.text());
         String userName = messageJSON.getString("userName");
         String content = messageJSON.getString("content");
-        Channel userChannel;
-        for (Map.Entry<Channel, String> entry : userChannelMap.entrySet()) {
-            userChannel = entry.getKey();
-            if (userChannel == currentChannel) {
-                userChannel.writeAndFlush(new TextWebSocketFrame("当前用户：" + content));
+        ChannelHandlerContext context;
+        for (Map.Entry<ChannelHandlerContext, String> entry : userChannelMap.entrySet()) {
+            context = entry.getKey();
+            if (context == ctx) {
+                context.writeAndFlush(new TextWebSocketFrame("当前用户：" + content));
             } else {
-                userChannel.writeAndFlush(new TextWebSocketFrame("用户[" + userName + "]：" + content));
+                context.writeAndFlush(new TextWebSocketFrame("用户[" + userName + "]：" + content));
             }
         }
 
-        userChannelMap.put(currentChannel, userName);
+        userChannelMap.put(ctx, userName);
     }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) {
-        Channel currentChannel = ctx.channel();
-        userChannelMap.put(currentChannel, "用户[" + currentChannel.remoteAddress() + "]");
+        userChannelMap.put(ctx, "用户[" + ctx.channel().remoteAddress() + "]");
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) {
-        Channel currentChannel = ctx.channel();
         String userName = null;
-        for (Map.Entry<Channel, String> entry : userChannelMap.entrySet()) {
-            if (entry.getKey() == currentChannel) {
+        for (Map.Entry<ChannelHandlerContext, String> entry : userChannelMap.entrySet()) {
+            if (entry.getKey() == ctx) {
                 userName = entry.getValue();
-                userChannelMap.remove(currentChannel);
+                userChannelMap.remove(ctx);
                 break;
             }
         }
 
         if (userName != null) {
-            for (Channel channel : userChannelMap.keySet()) {
-                channel.writeAndFlush(new TextWebSocketFrame("用户[" + userName + "] 离开聊天室"));
+            for (ChannelHandlerContext context : userChannelMap.keySet()) {
+                context.writeAndFlush(new TextWebSocketFrame("用户[" + userName + "] 离开聊天室"));
             }
         }
     }
